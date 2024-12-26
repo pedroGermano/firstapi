deb6b12a83751b1b098ab966263b84d5d447f997
Pedro Germano <pedrogermano232@gmail.com>
2024-12-26 07:29:19 -0300

    firstapi

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const http = require('http')
+const users = require('./mocks/users')
+
+const server = http.createServer((req, res) => {
+  console.log(`Request method: ${req.method} | Endpoint: ${req.url}`)
+
+  if (req.url === '/users' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(users))
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/html' })
+    res.end(`Cannot ${req.method} ${req.url}`)
+  }
+
+
+})
+
+server.listen(3000, () => console.log('Server started at http://localhost:3000'))
---
+++ src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Mateus'
+  },
+  {
+    id: 2,
+    name: 'Pedro'
+  },
+  {
+    id: 3,
+    name: 'Camilo'
+  },
+]
